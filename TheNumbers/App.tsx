d3f0cc18f4c4adafa5c9c2e2d248f9fc67bb6483
pado8 <floe9235@gmail.com>
2023-10-27 22:20:28 +0900

    태효/1027/버튼2번클릭

--- TheNumbers/App.tsx
+++ TheNumbers/App.tsx
@@ -4,7 +4,7 @@ import {
   NativeStackScreenProps,
 } from '@react-navigation/native-stack';
 import {NavigationContainer} from '@react-navigation/native';
-import { Image, Pressable, StyleSheet, Text, View } from "react-native";
+import { Image, Pressable, StyleSheet, Text, View, Alert } from "react-native";
 import BusSearchScreen from './src/pages/BusSearchScreen';
 import RouteGuideScreen from './src/pages/RouteGuideScreen';
 import SearchHistoryScreen from './src/pages/SearchHistoryScreen';
@@ -15,6 +15,7 @@ import {theme} from './src/assets/color';
 import busImg from './src/assets/images/bus.png';
 import settingImg from './src/assets/images/setting-button.png';
 import Icon from 'react-native-vector-icons/Ionicons';
+import Tts from 'react-native-tts';
 
 // 페이지의 목록
 type RootStackParamList = {
@@ -28,6 +29,18 @@ type HomeScreenProps = NativeStackScreenProps<RootStackParamList, 'Home'>;
 // type DetailsScreenProps = NativeStackScreenProps<ParamListBase, 'Details'>;
 
 function HomeScreen({navigation}: HomeScreenProps) {
+  const [count, setCount] = React.useState(0);
+  const onClick = () => {
+    if (count == 1) {
+      navigation.navigate('BusSearch');
+      setCount(0);
+    } else {
+      Tts.setDefaultLanguage('ko-KR');
+      Tts.speak('버스찾기');
+      setCount(count + 1);
+    }
+  };
+
   return (
     <View style={styles.container}>
       <Pressable onPress={() => navigation.navigate('Setting')} style={styles.settingButton}>
@@ -38,8 +51,10 @@ function HomeScreen({navigation}: HomeScreenProps) {
         <Image source={busImg} />
       </View>
       <View style={styles.screenButtonZoon}>
-        <Pressable
-          onPress={() => navigation.navigate('BusSearch')}
+        <Pressable 
+          onPress={() => {
+            onClick();
+          }}
           style={styles.screenButton}>
           <Text style={styles.screenButtonText}>버스찾기</Text>
         </Pressable>
